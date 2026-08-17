617a6960f3a401e7ca3201cf27b70cd458d1be1a
Augusto Emanuel Pereira <Augusto.Pereira@guarapuava.pr.gov.br>
2026-08-17 13:37:36 -0300

    variaveis lendo os inputs criadas

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const nomeInput = document.getElementById('nomeCompleto');
+const emailInput = document.getElementById('email');
+const telefoneInput = document.getElementById('telefone');
+
+function valNome() {
+
+}
