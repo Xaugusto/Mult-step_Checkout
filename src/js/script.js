13976f94665ce56e8709aa78645a16755bd4dd88
Seu Nome <seu-email@exemplo.com>
2026-08-23 18:07:34 -0300

    validacao do index termindada

--- src/js/script.js
+++ src/js/script.js
@@ -6,68 +6,50 @@ const botao = document.getElementById("formButton");
 function valNome() {
   if (nomeInput.value.trim() === "") {
     nomeInput.setCustomValidity("O nome é obrigatório!");
-  } else {
-    nomeInput.setCustomValidity("");
+    return false;
   }
-  nomeInput.reportValidity();
+  nomeInput.setCustomValidity("");
+  return true;
 }
 
 function valEmail() {
   if (emailInput.value.trim() === "") {
     emailInput.setCustomValidity("O email é obrigatório!");
+    return false;
   } else if (!emailInput.validity.valid) {
-    emailInput.setCustomValidity("Digiste um email válido!");
-  } else {
-    emailInput.setCustomValidity("");
+    emailInput.setCustomValidity("Digite um email válido!");
+    return false;
   }
-  emailInput.reportValidity();
+  emailInput.setCustomValidity("");
+  return true;
 }
 
 function valTelefone() {
   $("#telefone").mask("(00) 00000-0000");
   if (telefoneInput.value.trim() === "") {
     telefoneInput.setCustomValidity("O telefone é obrigatório!");
-  } else {
-    telefoneInput.setCustomValidity("");
+    return false;
   }
-  telefoneInput.reportValidity("");
-}
-
-function botaoStatus() {
-  const nomeValido = nomeInput.value.trim() !== "";
-  const emailValido =
-    emailInput.validity.valid && emailInput.value.trim() !== "";
-  const telefoneValido = telefoneInput.value.trim() !== "";
-
-  botao.disabled = !(nomeValido && emailValido && telefoneValido);
-}
-
-nomeInput.addEventListener("blur", () => {
-  valNome();
-  botaoStatus();
-});
-
-emailInput.addEventListener("blur", () => {
-  valEmail();
-  botaoStatus();
-});
-
-telefoneInput.addEventListener("blur", () => {
-  valTelefone();
-  botaoStatus();
-});
-
-nomeInput.addEventListener("input", () => {
-  nomeInput.setCustomValidity("");
-  botaoStatus();
-});
-
-emailInput.addEventListener("input", () => {
-  emailInput.setCustomValidity("");
-  botaoStatus();
-});
-
-telefoneInput.addEventListener("input", () => {
   telefoneInput.setCustomValidity("");
-  botaoStatus();
+  return true;
+}
+
+botao.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!valNome()) {
+    nomeInput.reportValidity();
+  }
+
+  if (!valEmail()) {
+    emailInput.reportValidity();
+  }
+
+  if (!valTelefone()) {
+    telefoneInput.reportValidity();
+  }
+
+  if (valNome() && valEmail() && valTelefone()) {
+    window.location.href = "src/pages/entrega.html";
+  }
 });
