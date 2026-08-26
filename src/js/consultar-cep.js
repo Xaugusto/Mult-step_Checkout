31a1d0d22e3403d433f3708a328b5fe8e635d2f3
Seu Nome <seu-email@exemplo.com>
2026-08-26 16:41:11 -0300

    funcao calcularCep() criada

--- src/js/consultar-cep.js
+++ src/js/consultar-cep.js
@@ -8,7 +8,18 @@ const estadoInput = document.getElementById("estado");
 
 const botaoConsulta = document.getElementById("calcCep");
 
-async function calcularCEP() {}
+async function calcularCEP() {
+  cepInput.value = "85067-050";
+  const url = `https://viacep.com.br/ws/${cepInput.value.replaceAll("-", "")}/json/`;
+
+  const resp = await fetch(url);
+  const dados = await resp.json();
+  console.log(dados);
+  enderecoInput.value = dados.logradouro;
+  bairroInput.value = dados.bairro;
+  cidadeInput.value = dados.localidade;
+  estadoInput.value = dados.uf;
+}
 
 botaoConsulta.addEventListener("click", function () {
   calcularCEP();
