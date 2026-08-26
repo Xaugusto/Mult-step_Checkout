4acba57e9f3a26ffa7cea7198cc33952ac65478a
Seu Nome <seu-email@exemplo.com>
2026-08-26 20:04:12 -0300

    mudado as variaveis

--- src/js/consultar-cep.js
+++ src/js/consultar-cep.js
@@ -1,24 +1,24 @@
-const cepInput = document.getElementById("CEP");
-const enderecoInput = document.getElementById("endereco");
-const numeroInput = document.getElementById("numero");
-const complementoInput = document.getElementById("complemento");
-const bairroInput = document.getElementById("bairro");
-const cidadeInput = document.getElementById("cidade");
-const estadoInput = document.getElementById("estado");
+const cepInput2 = document.getElementById("CEP");
+const enderecoInput2 = document.getElementById("endereco");
+const numeroInput2 = document.getElementById("numero");
+const complementoInput2 = document.getElementById("complemento");
+const bairroInput2 = document.getElementById("bairro");
+const cidadeInput2 = document.getElementById("cidade");
+const estadoInput2 = document.getElementById("estado");
 
 const botaoConsulta = document.getElementById("calcCep");
 
 async function calcularCEP() {
-  cepInput.value = "85067-050";
-  const url = `https://viacep.com.br/ws/${cepInput.value.replaceAll("-", "")}/json/`;
+  cepInput2.value = "85067-050";
+  const url = `https://viacep.com.br/ws/${cepInput2.value.replaceAll("-", "")}/json/`;
 
   const resp = await fetch(url);
   const dados = await resp.json();
   console.log(dados);
-  enderecoInput.value = dados.logradouro;
-  bairroInput.value = dados.bairro;
-  cidadeInput.value = dados.localidade;
-  estadoInput.value = dados.uf;
+  enderecoInput2.value = dados.logradouro;
+  bairroInput2.value = dados.bairro;
+  cidadeInput2.value = dados.localidade;
+  estadoInput2.value = dados.uf;
 }
 
 botaoConsulta.addEventListener("click", function () {
